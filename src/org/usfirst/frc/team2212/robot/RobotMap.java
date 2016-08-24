ea6b2bed67517b728be2095d79f8698c5bd692a4
Coral <Coral@DESKTOP-QI6C3HV>
2016-08-24 13:24:51 +0300

    Implemented driveTrain but usePIDOutput

--- src/org/usfirst/frc/team2212/robot/RobotMap.java
+++ src/org/usfirst/frc/team2212/robot/RobotMap.java
@@ -1,4 +1,11 @@
 package org.usfirst.frc.team2212.robot;
+
+
+import edu.wpi.first.wpilibj.DigitalSource;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.TalonSRX;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,10 +13,16 @@ package org.usfirst.frc.team2212.robot;
  * floating around.
  */
 public class RobotMap {
+
+	public static SpeedController elevatorMotor;
     // For example to map the left and right motors, you could define the
     // following variables to use with your drivetrain subsystem.
     // public static int leftMotor = 1;
     // public static int rightMotor = 2;
+	public static SpeedController leftDriveMotor = new TalonSRX(0);
+	public static SpeedController rightDriveMotor = new TalonSRX(0);
+	public static Encoder rightDrivePort;
+	public static Encoder leftDrivePort;
     
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
--- src/org/usfirst/frc/team2212/robot/subsystems/Drivetrain.java
+++ src/org/usfirst/frc/team2212/robot/subsystems/Drivetrain.java
@@ -1,34 +1,64 @@
 package org.usfirst.frc.team2212.robot.subsystems;
 
+import org.usfirst.frc.team2212.robot.RobotMap;
+
+import com.ni.vision.NIVision.GeometricSetupDataItem;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 /**
+ * 
  *
  */
 public class Drivetrain extends PIDSubsystem {
+	private static double i;
+	private static double d;
+	private static double p;
+	private static SpeedController leftMotor = RobotMap.leftDriveMotor;
+	private static SpeedController rightMotor = RobotMap.rightDriveMotor;
+	private Encoder rightPort = RobotMap.rightDrivePort;
+	private Encoder leftPort = RobotMap.leftDrivePort;
 
-    // Initialize your subsystem here
-    public Drivetrain() {
-        // Use these to get going:
-        // setSetpoint() -  Sets where the PID controller should move the system
-        //                  to
-        // enable() - Enables the PID controller.
-    }
-    
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    
-    protected double returnPIDInput() {
-        // Return your input value for the PID loop
-        // e.g. a sensor, like a potentiometer:
-        // yourPot.getAverageVoltage() / kYourMaxVoltage;
-    	return 0.0;
-    }
-    
-    protected void usePIDOutput(double output) {
-        // Use output to drive your system, like a motor
-        // e.g. yourMotor.set(output);
-    }
+
+	// Initialize your subsystem here
+	public Drivetrain() {
+    super( p,  i, d);
+    super.setSetpoint(1);
+    super.enable();
+    rightPort.setDistancePerPulse(0.01);
+    leftPort.setDistancePerPulse(0.01);
+		// Use these to get going:
+		// setSetpoint() - Sets where the PID controller should move the system
+		// to
+		// enable() - Enables the PID controller.
+	}
+	public static void drive(double left, double right){
+		leftMotor.set(left);
+		rightMotor.set(right);
+		
+	}
+		
+	
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+	protected double returnPIDInput() {
+		// Return your input value for the PID loop
+		// e.g. a sensor, like a potentiometer:
+		// yourPot.getAverageVoltage() / kYourMaxVoltage;
+		return rightPort.getDistance()+leftPort.getDistance()/2;
+	}
+
+	protected void usePIDOutput(double output) {
+		// Use output to drive your system, like a motor
+		// e.g. yourMotor.set(output);
+		
+	}
 }
